30f4cb8cc6b0ac656379aa795055500c38d58aac
Diego <cetecom@LC08L5701>
2017-05-16 11:05:18 -0400

    Sesiones

--- Revision.php
+++ Revision.php
@@ -1,2 +1,19 @@
 <?php
+include 'lib/Conexion.php';
+include 'lib/Usuario.php';
+
+
+$oUsr = new Usuario();
+
+$oUsr->nombre='nuevo';
+
+
+if($oUsr->VerificarUsuario())
+{
+    echo 'Existe';
+}
+else
+{
+    echo 'No existe';
+}
 
--- lib/Usuario.php
+++ lib/Usuario.php
@@ -3,5 +3,30 @@
         var $idUsuario;
         var $nombre;
         var $clave;
+        
+        //Valida la existencia del usuario
+        function VerificarUsuario(){
+            $oConexion = new Conexion();
+            if($oConexion->Conectar()){
+            
+                $db=$oConexion->objconn;
+                
+                $sql="Select * From acceso where nomusuario='$this->nombre'";
+                $resultado= $bd->query($sql);
+                if($resultado->num_rows>=1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else{
+            
+                return false;
+            }
+            }
+    
     }
 ?>
